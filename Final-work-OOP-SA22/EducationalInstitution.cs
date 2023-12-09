b510e274441c299643ab22bc6e17952396683d0d
Simplyfuze 31 <kondratuiksasha5@gmail.com>
2023-12-09 19:26:06 +0200

    Add project files.

---
+++ Final-work-OOP-SA22/EducationalInstitution.cs
@@ -0,0 +1,62 @@
+﻿
+
+namespace Final_work_OOP_SA22
+{
+    public abstract class EducationalInstitution
+    {
+
+        private string _name;
+        private string Name { get; set; }
+        private AccreditationLevels accreditationLevel;
+
+        private readonly DateTime foundationDate;
+
+        private int studentQuantity;
+
+        private Person headOfInstitution;
+
+        private int Rating;
+
+        private string phoneNumber;
+
+        public EducationalInstitution()
+        {
+        }
+
+        public EducationalInstitution(string name, AccreditationLevels accreditationLevel,
+            DateTime foundationDate, int studentQuantity, Person headOfInstitution,
+            int rating, string phoneNumber)
+        {
+            Name = name;
+            this.accreditationLevel = accreditationLevel;
+            this.foundationDate = foundationDate;
+            this.studentQuantity = studentQuantity;
+            this.headOfInstitution = headOfInstitution;
+            Rating = rating;
+            this.phoneNumber = phoneNumber;
+        }
+
+        public EducationalInstitution(EducationalInstitution institution)
+        {
+            Name = institution.Name;
+            this.accreditationLevel = institution.accreditationLevel;
+            this.foundationDate = institution.foundationDate;
+            this.studentQuantity = institution.studentQuantity;
+            this.headOfInstitution = institution.headOfInstitution;
+            Rating = institution.Rating;
+            this.phoneNumber = institution.phoneNumber;
+        }
+
+        public abstract void PrintInfo();
+    }
+
+    public enum AccreditationLevels
+    {
+        School = 1,
+        College,
+        Institute,
+        University
+    }
+
+
+}
---
+++ Final-work-OOP-SA22/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace Final_work_OOP_SA22
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public override string ToString()
+        {
+            return $"Name:{FirstName} {LastName}";
+        }
+    }
+}
