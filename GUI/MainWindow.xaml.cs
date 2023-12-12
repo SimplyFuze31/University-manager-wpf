deb3c0ced934384483d05f41b073f2b51fdad1ac
Simplyfuze 31 <kondratuiksasha5@gmail.com>
2023-12-12 03:05:43 +0200

    Add Gui to rep

---
+++ GUI/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Final_work_OOP_SA22;
+using GUI.Pages;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public delegate void DataGridDoubleClick(EducationalInstitution edu);
+        public DataGridDoubleClick DataGridClickHandler { get; set; }
+
+        public delegate void StarPageClick();
+        public StarPageClick StarPageClickHandler { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            StarPageClickHandler = OpenStarPage;
+            DataGridClickHandler = OpenChangeInstitutionDataPage;
+            OpenStarPage();
+            
+        }
+        
+        private void OpenChangeInstitutionDataPage(EducationalInstitution edu)
+        {
+            Main.Content = new ChangeInstitutionDataPage(edu, StarPageClickHandler);
+        }
+
+        private void OpenStarPage()
+        {
+            Main.Content = new StartPage(DataGridClickHandler);
+        }
+        private void AddButton_OnClick(object sender, RoutedEventArgs e)
+        {
+        
+        }
+    }
+}
